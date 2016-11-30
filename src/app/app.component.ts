562bc35720299cf8b99cbe92bba32bcaca0524b3
Matthieu Faugere <matthieu.faugere@gmail.com>
2016-11-30 10:45:00 +0100

    add urls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  urls = [
+    'https://pbs.twimg.com/profile_images/619573624903761920/EGZ2I6wG.jpg',
+    'https://static-s.aa-cdn.net/img/gp/20600002144900/QcM5ze2mGK0frV4cbdL7otLHts8p_RoC-N2mggz7M6Jv36vZN3B9Y3OmFvJwwLHuUyDc=w300?v=1',
+    'https://pbs.twimg.com/profile_images/632658152505364480/txMFRoCf.jpg',
+  ];
 }
